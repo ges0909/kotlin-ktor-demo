900932b47b4c5e78d462ac8387547eababeb4aca
Gerrit Schrader <gerrit.schrader@infinit-services.de>
2019-05-16 10:55:37 +0200

    async database access with coroutines

--- src/main/kotlin/de/schrader/ktor/repository/PersonRepository.kt
+++ src/main/kotlin/de/schrader/ktor/repository/PersonRepository.kt
@@ -1,6 +1,8 @@
 package de.schrader.ktor.repository
 
 import de.schrader.ktor.controller.Person
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -12,32 +14,42 @@ class PersonRepository {
         val age = integer("age")
     }
 
-    fun all(): List<Person> = transaction {
-        DatabaseTable.selectAll().map { it.toPerson() }
-    }
-
-    fun get(id: Int): Person = transaction {
-        DatabaseTable.select { DatabaseTable.id eq id }.first().toPerson()
-    }
-
-    fun create(person: Person): Int = transaction {
-        DatabaseTable.insert {
-            it[name] = person.name
-            it[age] = person.age
-        } get DatabaseTable.id
-    }
-
-    fun update(id: Int, person: Person) = transaction {
-        DatabaseTable.update({ DatabaseTable.id eq id }) {
-            it[name] = person.name
-            it[age] = person.age
+    suspend fun all(): List<Person> = withContext(Dispatchers.IO) {
+        transaction {
+            DatabaseTable.selectAll().map { it.toPerson() }
         }
-        Unit
     }
 
-    fun delete(id: Int) = transaction {
-        DatabaseTable.deleteWhere { DatabaseTable.id eq id }
-        Unit
+    suspend fun get(id: Int): Person = withContext(Dispatchers.IO) {
+        transaction {
+            DatabaseTable.select { DatabaseTable.id eq id }.first().toPerson()
+        }
+    }
+
+    suspend fun create(person: Person): Int = withContext(Dispatchers.IO) {
+        transaction {
+            DatabaseTable.insert {
+                it[name] = person.name
+                it[age] = person.age
+            } get DatabaseTable.id
+        }
+    }
+
+    suspend fun update(id: Int, person: Person) = withContext(Dispatchers.IO) {
+        transaction {
+            DatabaseTable.update({ DatabaseTable.id eq id }) {
+                it[name] = person.name
+                it[age] = person.age
+            }
+            Unit
+        }
+    }
+
+    suspend fun delete(id: Int) = withContext(Dispatchers.IO) {
+        transaction {
+            DatabaseTable.deleteWhere { DatabaseTable.id eq id }
+            Unit
+        }
     }
 
     private fun ResultRow.toPerson() = Person(
--- src/main/kotlin/de/schrader/ktor/service/PersonService.kt
+++ src/main/kotlin/de/schrader/ktor/service/PersonService.kt
@@ -2,27 +2,28 @@ package de.schrader.ktor.service
 
 import de.schrader.ktor.controller.Person
 import de.schrader.ktor.repository.PersonRepository
+import kotlinx.coroutines.withContext
 
 interface PersonService {
-    fun all(): List<Person>
-    fun get(id: Int): Person
-    fun create(person: Person): Person
-    fun update(id: Int, person: Person)
-    fun delete(id: Int)
+    suspend fun all(): List<Person>
+    suspend fun get(id: Int): Person
+    suspend fun create(person: Person): Person
+    suspend fun update(id: Int, person: Person)
+    suspend fun delete(id: Int)
 }
 
 class PersonServiceImpl(private val personRepository: PersonRepository) : PersonService {
 
-    override fun all(): List<Person> = personRepository.all()
+    override suspend fun all(): List<Person> = personRepository.all()
 
-    override fun get(id: Int): Person = personRepository.get(id)
+    override suspend fun get(id: Int): Person = personRepository.get(id)
 
-    override fun create(person: Person): Person {
+    override suspend fun create(person: Person): Person {
         val id = personRepository.create(person)
         return get(id)
     }
 
-    override fun update(id: Int, person: Person) = personRepository.update(id, person)
+    override suspend fun update(id: Int, person: Person) = personRepository.update(id, person)
 
-    override fun delete(id: Int) = personRepository.delete(id)
+    override suspend fun delete(id: Int) = personRepository.delete(id)
 }
