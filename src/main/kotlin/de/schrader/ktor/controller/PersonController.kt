e7789ae9b51702a768eb4473d6c63c7918e19847
Gerrit Schrader <gerrit.schrader@infinit-services.de>
2019-05-22 17:26:27 +0200

    refactored

--- src/main/kotlin/de/schrader/ktor/controller/PersonController.kt
+++ src/main/kotlin/de/schrader/ktor/controller/PersonController.kt
@@ -1,5 +1,6 @@
 package de.schrader.ktor.controller
 
+import arrow.core.getOrElse
 import de.schrader.ktor.None
 import de.schrader.ktor.Some
 import de.schrader.ktor.repository.Person
@@ -7,7 +8,6 @@ import de.schrader.ktor.service.PersonService
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
-import io.ktor.locations.locations
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.*
@@ -21,16 +21,13 @@ fun Route.persons() {
     route("/persons") {
 
         get {
-            when (val option = personService.all()) {
-                is Some<*> -> call.respond(HttpStatusCode.OK, option.value)
-                is None<*> -> call.respond(HttpStatusCode.InternalServerError)
-            }
+            call.respond(HttpStatusCode.OK, personService.all().getOrElse { "" })
         }
 
         get("/{id}") {
             val id = call.parameters["id"]!!.toInt()
             when (val option = personService.read(id)) {
-                is Some<*> -> call.respond(HttpStatusCode.OK, option.value)
+                is Some<*> -> call.respond(HttpStatusCode.OK, option.getOrElse { "" })
                 is None<*> -> call.respond(HttpStatusCode.NotFound)
             }
         }
@@ -39,8 +36,8 @@ fun Route.persons() {
             val person = call.receive<Person>()
             when (val option = personService.create(person)) {
                 is Some<*> -> {
-                    val path = locations.href(option.value)
-                    call.respond(HttpStatusCode.Created, option.value)
+                    // val path = locations.href(option.getOrElse { "" })
+                    call.respond(HttpStatusCode.Created, option.getOrElse { "" })
                 }
                 is None<*> -> call.respond(HttpStatusCode.InternalServerError)
             }
